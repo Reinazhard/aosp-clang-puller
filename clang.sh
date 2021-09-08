0fd9a573d66ee8391cfc3ab5a059df2390641202
Reinazhard <reinazhard@gmail.com>
2021-09-08 10:17:47 +0000

    Downgrade HTTP version to 1.1
    "error: RPC failed; curl 92 HTTP/2 stream 0 was not closed cleanly: CANCEL (err 8)"
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- clang.sh
+++ clang.sh
@@ -5,6 +5,8 @@
 export EMOH="/drone/src"
 export CLANG_VERSION="clang-r433403"  # https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+/refs/heads/master
 
+git config --global http.version HTTP/1.1
+
 mkdir ${EMOH}/tmp
 cd ${EMOH}/tmp
 
