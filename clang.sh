198c870de482efdca778afd1076d1c9d4fee6024
Arta Reina <reinazhard@gmail.com>
2022-03-22 00:29:35 +0700

    14.0.3
    r450784

--- clang.sh
+++ clang.sh
@@ -3,7 +3,7 @@
 
 
 export EMOH="/drone/src"
-export CLANG_VERSION="clang-r445002"  # https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+/refs/heads/master
+export CLANG_VERSION="clang-r450784"  # https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+/refs/heads/master
 
 mkdir ${EMOH}/tmp
 cd ${EMOH}/tmp
